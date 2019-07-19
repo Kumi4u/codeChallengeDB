e1f9e5a78e02a89dc87c18b1485310c76c729bbf
Kumar Saurabh <kumar.saurabh@zelfi.com>
2019-07-19 12:25:46 +0200

    update colors

---
+++ app/src/main/java/com/db/post/postbook/screen/posts/UserPostActivity.kt
@@ -0,0 +1,13 @@
+package com.db.post.postbook.screen.posts
+
+import android.os.Bundle
+import com.db.post.postbook.R
+import com.db.post.postbook.base.BaseActivity
+
+class UserPostActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_post)
+    }
+}
