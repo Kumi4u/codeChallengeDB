5b493c0e7a06418acdbc28b3ca961b43e1d42a60
Kumar Saurabh <kumar.saurabh@zelfi.com>
2019-07-19 16:11:01 +0200

    fix bottom nav bar visibility

--- app/src/main/java/com/db/post/postbook/screen/MainActivity.kt
+++ app/src/main/java/com/db/post/postbook/screen/MainActivity.kt
@@ -1,10 +1,13 @@
-package com.db.post.postbook
+package com.db.post.postbook.screen
 
 import android.os.Bundle
+import android.view.View
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
+import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.db.post.postbook.R
 import com.db.post.postbook.base.BaseActivity
 import com.db.post.postbook.databinding.ActivityMainBinding
 
@@ -14,12 +17,20 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         dataBinding.bottomNavigationView.setupWithNavController(findNavController(R.id.nav_fragment))
+
+        findNavController(R.id.nav_fragment).addOnDestinationChangedListener { _, destination, _ ->
+
+            dataBinding.bottomNavigationView.visibility =
+                if (destinationIsPost(destination)) View.VISIBLE else View.GONE
+        }
     }
 
     override fun doDataBinding(): ViewDataBinding {
         dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         return dataBinding
-
     }
 
+    private fun destinationIsPost(destination: NavDestination): Boolean {
+        return destination.id == R.id.userFavPostFragment || destination.id == R.id.userPostFragment
+    }
 }
